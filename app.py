84fbfedfba3309395490953497649f3e4cee26f1
Jackson Galan <47265694+abstractionjackson@users.noreply.github.com>
2024-03-18 17:46:11 +0000

    add render template function nd write to output

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
-from os import environ
+from os import environ, path
 from orgparse import load as load_org
+from jinja2 import Environment, FileSystemLoader
 
 load_dotenv()
 
@@ -19,10 +20,22 @@ def get_title(node):
     meta_data = get_meta_data(node.body)
     return meta_data["title"]
 
+def render_template(template_name, data):
+    # Set up Jinja environment
+    template_dir = path.join(path.dirname(path.abspath(__file__)), 'templates')  # Change 'templates' to your template directory
+    env = Environment(loader=FileSystemLoader(template_dir))
+
+    # Load the template
+    template = env.get_template(template_name)
+
+    # Render the template with data
+    return template.render(data)
+
 def main():
     print("App")
     mode = environ.get('MODE')
     data = None
+    template = environ.get("TEMPLATE")
     if mode.lower() == "example":
         # load the sample resume
         print("Loading the sample resume...")
@@ -35,6 +48,10 @@ def main():
         raise Exception()
     title = get_title(data)
     print("Title: ", title)
+    template = render_template(environ.get("TEMPLATE"), {"title": title})
+    
+    with open(path.join("public", environ.get("OUTPUT")), 'w') as f:
+        f.write(template)
 
 if __name__ == "__main__":
     main()
