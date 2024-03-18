0901ceccb6baf0cd512d758310a0a6ba9247ba35
Jackson Galan <47265694+abstractionjackson@users.noreply.github.com>
2024-03-18 16:05:13 +0000

    init

---
+++ watcher.py
@@ -0,0 +1,31 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import subprocess
+
+class HTMLHandler(FileSystemEventHandler):
+    def __init__(self):
+        super(HTMLHandler, self).__init__()
+
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+        elif event.event_type == 'modified' and event.src_path.endswith('.html'):
+            print(f"HTML file {event.src_path} has been modified. Relaunching app.py...")
+            subprocess.run(['python', 'app.py'])
+
+if __name__ == "__main__":
+    path = "templates"  # Change to the directory you want to monitor
+    event_handler = HTMLHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+
+    print(f"Watching directory {path} for changes...")
+
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
